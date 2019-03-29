e3501b1fde5bbada053e4dd443912384cf378470
panos <panvarnavas@gmail.com>
2019-03-29 15:14:08 +0200

    Updates

---
+++ precache-manifest.decf30063c691139826659eb40a05cdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59b994fe6cb40dbf3519",
+    "url": "/react-website/static/js/runtime~main.5bd11a58.js"
+  },
+  {
+    "revision": "ae5830720a4551fac419",
+    "url": "/react-website/static/js/main.f44778c0.chunk.js"
+  },
+  {
+    "revision": "f3a1184cc06fc0bd11b4",
+    "url": "/react-website/static/js/2.7f4455fe.chunk.js"
+  },
+  {
+    "revision": "ae5830720a4551fac419",
+    "url": "/react-website/static/css/main.4f429074.chunk.css"
+  },
+  {
+    "revision": "6c134935e65dd0cebf550039bd5e80ae",
+    "url": "/react-website/index.html"
+  }
+];
